280b59578e285c8556e872640a2b3b098d27e709
Jason Coposky <jasonc@renci.org>
2019-09-25 10:38:15 -0400

    add usage for project_collection_creation

--- policy/project_collection_creation.r
+++ policy/project_collection_creation.r
@@ -1,3 +1,6 @@
+# Example Usage
+# irule -F project_collection_creation.r "*proj_name='alice-project'" "*owner='alice'" "*collab_list='bobby, eve , joe'" *lifetime=0.001 *object_quota=5 *size_quota=500
+
 project_collection_creation {
     create_project_collection(
         *proj_name, *owner, *collab_list,
